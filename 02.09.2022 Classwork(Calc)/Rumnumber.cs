30ccbd7e97e61dfd1545dda93d3f73dcb499fcf4
tim <1chloeprice@gmail.com>
2022-09-07 12:29:03 +0300

    ав

--- 02.09.2022 Classwork(Calc)/Rumnumber.cs
+++ 02.09.2022 Classwork(Calc)/Rumnumber.cs
@@ -8,73 +8,44 @@ namespace Classwork
 { // класс для римских чисел
     public class Rumnumber
     {
-        public static int Parse(string str)
+        // Одержання числа з рядкового запису
+        public static int Parse(String str)
         {
-
-
-
             char[] digits = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
             int[] digitValues = { 1, 5, 10, 50, 100, 500, 1000 };
-            int res = 0;
-            //Если следующая цифра числа больше текущей, то
-            // она вычитается из результата, иначе добавляется
-            //IX: -1 + 10; XC: -10+100; XX: +10+10; CX : +100+10
-            for (int i = str.Length - 1; i > -1; i--)
+            // Якщо наступна цифра числа більша за поточну, то
+            // вона віднімається від результату, інакше додається
+            // IX : -1 + 10;  XC : -10 + 100;  XX : +10+10; CX : +100+10
+            // XIV : +10-1+5; XIIII : 10+1+1+1+1
+
+            int pos = str.Length - 1;  // позиція останньої цифри числа
+            char digit = str[pos];     // символ цифри
+            int ind = Array.IndexOf(digits, digit);  // позиція цифри у масиві
+            if (ind == -1)
             {
-
-                char digit = str[i];
-                char digit2 = str[i];
-
-                if (i - 1 > -1)
-                {
-                    digit2 = str[i - 1];
-                }
-
-
-
-
-
-
-
-                int ind = Array.IndexOf(digits, digit);
-                int ind2 = Array.IndexOf(digits, digit2);
-
-
+                throw new ArgumentException($"Invalid char {digit}");
+            }
+            int val = digitValues[ind];  // величина цифри
+            int res = val;
+            int nextDigitVal = val;
+            while (pos > 0)
+            {
+                pos -= 1;  // передостання цифра у числі
+                           // Визначаємо величину цифри
+                           // порівнюємо з наступною (останньою) цифрою (одержана вище)
+                           // додаємо або віднімаємо в залежності від рез. порівняння
+                           // продовжуємо доки pos не дійде до 0
+                digit = str[pos];     // символ цифри
+                ind = Array.IndexOf(digits, digit);  // позиція цифри у масиві
                 if (ind == -1)
                 {
-                    throw new ArgumentException($"Invalid character {digit}");
+                    throw new ArgumentException($"Invalid char {digit}");
                 }
-                int val = digitValues[ind];
-
-                int val2 = digitValues[ind2];
-                
-                if (i <= 0) { val2 = 0; }
-
-                if (val <= val2)
-                {
-
-
-                    res += val + val2;
-
-
-                }
-                else
-                {
-
-
-                    res += val - val2;
-
-
-
-
-                }
-                if (i != 0)
-                {
-                    i--;
-                }
-
-
-                //  pos -= 1;// предпоследняя цифра
+                val = digitValues[ind];  // величина цифри
+                res += (val < nextDigitVal)
+                        ? -val
+                        : val;
+                nextDigitVal = val;
             }
             return res;
         }
