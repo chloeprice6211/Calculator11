40526567fa5beaba8467f917cc850ff43a11c335
tim <1chloeprice@gmail.com>
2022-09-14 17:41:36 +0300

    upgraded

--- 02.09.2022 Classwork(Calc)/Rumnumber.cs
+++ 02.09.2022 Classwork(Calc)/Rumnumber.cs
@@ -6,54 +6,159 @@ using System.Threading.Tasks;
 
 namespace Classwork
 { // класс для римских чисел
-    public class Rumnumber
+    public record Rumnumber
     {
-        // Одержання числа з рядкового запису
+
+        public int a { get; set; }
+
+        public Rumnumber(int A = 0)
+        {
+            a = A;
+        }
+
+
+
+
+
         public static int Parse(String str)
         {
-
-            if (str.Contains('N') && str.Length > 1)
+            if (str == null)
             {
-                throw new ArgumentException("Only 1 N is allowed!");
+                throw new ArgumentNullException();
+            }
+            if (str == "N")
+            {
+                return 0;
+            }
+
+            bool isNegative = false;
+            if (str.StartsWith('-'))
+            {
+                isNegative = true;
+                str = str[1..];
+            }
+
+            if (str.Length < 1)
+            {
+                throw new ArgumentException("Empty string not allowed");
             }
 
             char[] digits = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
             int[] digitValues = { 1, 5, 10, 50, 100, 500, 1000 };
-            // Якщо наступна цифра числа більша за поточну, то
-            // вона віднімається від результату, інакше додається
-            // IX : -1 + 10;  XC : -10 + 100;  XX : +10+10; CX : +100+10
-            // XIV : +10-1+5; XIIII : 10+1+1+1+1
 
-            int pos = str.Length - 1;  // позиція останньої цифри числа
-            char digit = str[pos];     // символ цифри
-            int ind = Array.IndexOf(digits, digit);  // позиція цифри у масиві
+            int pos = str.Length - 1;
+            char digit = str[pos];
+            int ind = Array.IndexOf(digits, digit);
             if (ind == -1)
             {
                 throw new ArgumentException($"Invalid char {digit}");
             }
-            int val = digitValues[ind];  // величина цифри
+            int val = digitValues[ind];
             int res = val;
             int nextDigitVal = val;
             while (pos > 0)
             {
-                pos -= 1;  // передостання цифра у числі
-                           // Визначаємо величину цифри
-                           // порівнюємо з наступною (останньою) цифрою (одержана вище)
-                           // додаємо або віднімаємо в залежності від рез. порівняння
-                           // продовжуємо доки pos не дійде до 0
-                digit = str[pos];     // символ цифри
-                ind = Array.IndexOf(digits, digit);  // позиція цифри у масиві
+                pos -= 1;
+                digit = str[pos];
+                ind = Array.IndexOf(digits, digit);
                 if (ind == -1)
                 {
                     throw new ArgumentException($"Invalid char {digit}");
                 }
-                val = digitValues[ind];  // величина цифри
+                val = digitValues[ind];
                 res += (val < nextDigitVal)
                         ? -val
                         : val;
                 nextDigitVal = val;
             }
+            return isNegative ? -res : res;
+        }
+
+        public Rumnumber Add(Rumnumber rn)
+        {
+
+            if (rn is null)
+            {
+                throw new ArgumentException(nameof(rn));
+            }
+
+            return new(this.a + rn.a);
+
+        }
+        public Rumnumber Add(int rn)
+        {
+
+
+
+            return new(this.a + rn);
+
+        }
+        public Rumnumber Add(string roman)
+        {
+            return this.Add(new Rumnumber(Parse(roman)));
+        }
+        public Rumnumber Add(Rumnumber f, int s)
+        {
+            return this.Add(new Rumnumber(s));
+        }
+        public static Rumnumber Add(Rumnumber f, string s)
+        {
+            return new Rumnumber(f.a + Parse(s));
+        }
+        public static Rumnumber Add(string f, string s)
+        {
+            return new Rumnumber(Parse(f) + Parse(s));
+        }
+        public static Rumnumber Add(Rumnumber f, Rumnumber s)
+        {
+            return new Rumnumber(f.a + s.a);
+        }
+        public static Rumnumber Add(int f, int s)
+        {
+            return new Rumnumber(f).Add(s);
+        }
+
+        public static Rumnumber Add(object obj1, object obj2)
+        {
+            var rns = new Rumnumber[] { null!, null! };
+            var pars = new object[] { obj1, obj2 };
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (pars[i] is null) throw new ArgumentNullException($"obj{i + 1}");
+
+                if (pars[i] is int val) rns[i] = new Rumnumber(val);
+                else if (pars[i] is String str) rns[i] = new Rumnumber(Parse(str));
+                else if (pars[i] is Rumnumber rn) rns[i] = rn;
+                else throw new ArgumentException($"obj{i + 1}: type unsupported");
+            }
+
+            return rns[0].Add(rns[1]);   // заменить на цикл
+        }
+
+        public override string ToString()
+        {
+            if (this.a == 0)
+            {
+                return "N";
+            }
+            int n = this.a < 0 ? -this.a : this.a;
+            String res = this.a < 0 ? "-" : "";
+
+            String[] parts = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+            for (int j = 0; j <= parts.Length - 1; j++)
+            {
+                while (n >= values[j])
+                {
+                    n -= values[j];
+                    res += parts[j];
+                }
+            }
+
             return res;
         }
     }
+
 }
--- TestPca/AppTest.cs
+++ TestPca/AppTest.cs
@@ -12,7 +12,7 @@ namespace TestProject
         public void CalcTest()
         {
             // Спроба створити об'єкт головного класу
-          Classwork.Calc calc = new();
+            Classwork.Calc calc = new();
             // Твердження - маємо одержати не-null результат
             Assert.IsNotNull(calc);
         }
@@ -44,13 +44,14 @@ namespace TestProject
         public void RomanNumberParse3MoreDigits()
         {
             Assert.AreEqual(30, Rumnumber.Parse("XXX"));
-            Assert.AreEqual(401, Rumnumber.Parse("CDI"));
+            // Assert.AreEqual(401, Rumnumber.Parse("CDI"));
             Assert.AreEqual(1999, Rumnumber.Parse("MCMXCIX"));
         }
-       public void RomanNumberParseInvalidDigit()
+        [TestMethod]
+        public void RomanNumberParseInvalidDigit()
 
         {
-          var exc=  Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("XXA"); });
+            var exc = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("XXA"); });
             Assert.ThrowsException<Exception>(() => { Rumnumber.Parse("CDI"); });
             var exp = new ArgumentException("Invalid char A");
 
@@ -59,7 +60,7 @@ namespace TestProject
             Assert.AreEqual(exp, exc);
 
 
-             exc = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("X11"); });
+            exc = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("X11"); });
             exp = new ArgumentException("Invalid char 11");
 
             Assert.AreEqual(exp.Message, exc.Message);
@@ -70,44 +71,176 @@ namespace TestProject
             exc = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("X X"); });
 
             exp = new ArgumentException("Invalid char X X");
-            Assert.AreEqual(exc.Message.StartsWith(exp.Message),true);
+            Assert.AreEqual(exc.Message.StartsWith(exp.Message), true);
 
 
 
         }
         [TestMethod]
         public void RomanNumberParseEmpty()
-        { Rumnumber.Parse("");
+        {
+            Rumnumber.Parse("");
 
 
             Rumnumber.Parse(null!);
-            var exp = new ArgumentException("Invalid char ");
+            var exp = new ArgumentException("Empty string is not allowed");
             var exc = Assert.ThrowsException<Exception>(() => Rumnumber.Parse(""), exp.Message);
-                exc = Assert.ThrowsException<Exception>(() => Rumnumber.Parse(null!),exp.Message);
-           
-           
+            exc = Assert.ThrowsException<Exception>(() => Rumnumber.Parse(null!), exp.Message);
+
+
         }
 
-        // task 2
+        //- Додати тест для числа "N" - 0
         [TestMethod]
-        public void RomanNumberParseNPlacement()
+        public void RomNumberNTest()
         {
-            // ~ XN  
-            var exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("XN"); });
-            var expect = new ArgumentException("Only 1 N is allowed!");
+            Assert.AreEqual(0, Rumnumber.Parse("N"));
+        }
+        [TestMethod]
+        public void RomanNumberCtor()
+        {
+            Rumnumber rum = new();
+            rum = new(10);
+            rum = new(0);
+        }
+        [TestMethod]
+        public void RomanNumberToString()
+        {
+            Rumnumber romanNumber = new();
+            Assert.AreEqual("N", romanNumber.ToString());
+
+
+
+            romanNumber = new(10);
+            Assert.AreEqual("X", romanNumber.ToString());
+
+
+
+            romanNumber = new(90);
+            Assert.AreEqual("XC", romanNumber.ToString());
+
+
+
+            romanNumber = new(20);
+            Assert.AreEqual("XX", romanNumber.ToString());
+
+
+
+            romanNumber = new(1999);
+            Assert.AreEqual("MCMXCIX", romanNumber.ToString());
+        }
+        [TestMethod]
+        public void RomanNumberTostringParse()
+        {
+            Rumnumber num = new();
+            for (int n = 0; n <= 100; n++)
+            {
+                num.a = n;
+                Assert.AreEqual(n, Rumnumber.Parse(num.ToString()));
+            }
+        }
+        [TestMethod]
+        public void RomanNumbTypeTest()
+        {
+
+
+            Rumnumber rm1 = new(10);
+            Rumnumber rm2 = rm1;
+            Assert.AreSame(rm1, rm2);
+            Rumnumber rm3 = rm1 with { };
+            Assert.AreNotSame(rm3, rm1);
+            Assert.AreEqual(rm3, rm1);
+            Assert.IsTrue(rm3 == rm1);
+
+            Rumnumber rm4 = rm1 with { a = 10 };
+
+            Assert.AreNotSame(rm4, rm1);
+            Assert.IsFalse(rm4 == rm1);
+            Assert.AreNotEqual(rm4, rm1);
+            Assert.IsFalse(rm1.Equals(rm4));
+        }
+        [TestMethod]
+        public void rumanNegative()
+        {
+            var exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("XXA"); });
+
+            var expect = new ArgumentException("Invalid char A");
+
             Assert.AreEqual(expect.Message, exception.Message);
 
-            // XNX 
-            exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("XNX"); });
+            exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("MCMBCIX"); });
+            expect = new ArgumentException("Invalid char B");
             Assert.AreEqual(expect.Message, exception.Message);
 
-            //  NX
-            exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("NX"); });
+            exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("WIII"); });
+            expect = new ArgumentException("Invalid char W");
             Assert.AreEqual(expect.Message, exception.Message);
+
+            exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("DCCS"); });
+            expect = new ArgumentException("Invalid char S");
+            Assert.AreEqual(expect.Message, exception.Message);
+
+
+            exception = Assert.ThrowsException<ArgumentException>(() => { Rumnumber.Parse("X X"); });
+            Assert.AreEqual(exception.Message.StartsWith("Invalid char"), true);
+        }
+
+        [TestMethod]
+        public void AddvaluesTest()
+        {
+            // Rumnumber a = new() { a = 10 };
+
+
+
+            Rumnumber romanNumber = new Rumnumber(10);
+            Assert.AreEqual(20, romanNumber.Add(10).a);
+            Assert.AreEqual("V", romanNumber.Add(-5).ToString());
+            Assert.AreEqual(romanNumber, romanNumber.Add(0));
+
+        }
+        [TestMethod]
+        public void AddstringTest()
+        {
+
+
+            Assert.AreEqual(-10, Rumnumber.Parse("-X"));
+            Assert.AreEqual(-400, Rumnumber.Parse("-CD"));
+            Assert.AreEqual(-1900, Rumnumber.Parse("-MCM"));
+
+            Rumnumber rn = new() { a = -10 };
+            Assert.AreEqual("-X", rn.ToString());
+            rn.a = -90;
+            Assert.AreEqual("-XC", rn.ToString());
+
+            Assert.ThrowsException<ArgumentException>(() => Rumnumber.Parse("M-CM"));
+            Assert.ThrowsException<ArgumentException>(() => Rumnumber.Parse("M-"));
+            Assert.ThrowsException<ArgumentException>(() => Rumnumber.Parse("-"));
+            Assert.ThrowsException<ArgumentException>(() => Rumnumber.Parse("-N"));
+            Assert.ThrowsException<ArgumentException>(() => Rumnumber.Parse("--X"));
+            Assert.ThrowsException<ArgumentException>(() => Rumnumber.Parse("-C-X"));
+            Assert.ThrowsException<ArgumentException>(() => Rumnumber.Parse("--"));
+
+
+
+        }
+        [TestMethod]
+        public void AddStaticTest()
+        {
+            Rumnumber rn5 = Rumnumber.Add(2, 3);
+            Rumnumber rn8 = Rumnumber.Add(rn5, 3);
+            Rumnumber rn10 = Rumnumber.Add("I", "IX");
+            Rumnumber rn9 = Rumnumber.Add(rn5, "IV");
+            Rumnumber rn13 = Rumnumber.Add(rn5, rn8);
+
 
         }
 
+    }
 
+
+    [TestClass]
+    public class OperationTest
+    {
     }
 }
 
